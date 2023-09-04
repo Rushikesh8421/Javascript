4a949039e36f29e0fd9e1339303aad4537e28a5e
Rushikesh8421 <patilrushikesh8421@gmail.com>
2023-09-04 23:22:00 +0530

    Getting started with javascript from scratch

--- oop.js
+++
@@ -1,9 +0,0 @@
-function Circle (radius){
-  this.radius = radius;
-}
-Circle.prototype.getArea = function(){
-  return Math.PI * Math.pow(this.radius, 2);
-}
-x = prompt("Enter the radius: ");
-var mycircle = new Circle(x);
-console.log(mycircle.getArea());
